b49423e947a1a5d636ae5759f5fd3585f0841d0c
jeeva <jeevaprakashdr@gmail.com>
2023-12-07 22:33:40 +0100

    Deploying cluster of webserver
    Create a launch configuration for ASG enabling blue green deployment.

--- main.tf
+++ main.tf
@@ -7,6 +7,18 @@ variable "server_port" {
   type = number
   default = 8080
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-first-instance-security-group"
 
@@ -18,19 +30,31 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "example" {
-    ami                  = "ami-0fb653ca2d3203ac1"
-    instance_type        = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
-    user_data            = <<-EOF
+resource "aws_launch_configuration" "example" {
+  image_id              = "ami-0fb653ca2d3203ac1"
+  instance_type         = "t2.micro"
+  security_groups       = [aws_security_group.instance.id]
+
+  user_data            = <<-EOF
                 #!/bin/bash
                 echo "well now you begin" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-    user_data_replace_on_change = true
+resource "aws_autoscaling_group" "example" {
+  launch_configuration      = aws_launch_configuration.example.name
+  vpc_zone_identifier       = data.aws_subnets.default.ids
 
-    tags = {
-      Name = "terraform-first-instance"
-    }
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "name"
+    value = "auto_scaling_group_example"
+    propagate_at_launch = true
+  }
 }
