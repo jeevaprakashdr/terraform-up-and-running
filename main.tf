fa60b0ffbc96e70f583056f8fa017fa068d229b2
jeeva <jeevaprakashdr@gmail.com>
2024-01-05 14:34:46 +0100

    Deploying cluster of webservers

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ provider "aws" {
     region = "us-east-2"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-webserver-security-group"
 
@@ -13,6 +24,34 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_launch_configuration" "webserver" {
+  image_id = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  security_groups = [ aws_security_group.instance.id ]
+  
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello learner from AWS" >> index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "webserver" {
+    launch_configuration = aws_launch_configuration.webserver.name
+    vpc_zone_identifier = data.aws_subnets.default.ids
+    
+    min_size = 2
+    max_size = 10
+
+    tag {
+      key = "Name"
+      value = "terraform_autoscalling_webserver"
+      propagate_at_launch = true
+    }
+}
 resource "aws_instance" "webserver" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
