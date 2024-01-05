ff7cb92cc80895bb7a21815fde0fae2f7c9c3267
jeeva <jeevaprakashdr@gmail.com>
2024-01-05 14:06:26 +0100

    Deploying configurable webserver
    Moved server port number to a variable file

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-webserver-security-group"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
@@ -21,7 +21,7 @@ resource "aws_instance" "webserver" {
   user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello learner from AWS" >> index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
   
   user_data_replace_on_change = true
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "server_port" {
+  description = "The port of webserver serving all the http requests"
+  type = number
+  default = 8080
+}
