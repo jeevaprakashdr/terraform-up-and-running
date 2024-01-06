12fec0c183154c452e10271e22f3e35fe50cfeba
jeeva <jeevaprakashdr@gmail.com>
2024-01-07 00:12:48 +0100

    adding scheduled auto scaling

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -14,3 +14,23 @@ module "webserver_cluster" {
   max_size = 10
   server_port = var.server_port
 }
+
+resource "aws_autoscaling_schedule" "scale_out_during_working_hours" {
+  scheduled_action_name = "scale_out_during_working_hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_during_evenings" {
+  scheduled_action_name = "scale_out_during_working_hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
